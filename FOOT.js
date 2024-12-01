88aca3570b5cb33de09c7dd2245260ee8c03887f
hamzalhadouchi <hamzabniss16@gmail.com>
2024-12-01 19:55:41 +0100

    Optimize card styling

--- FOOT.js
+++ FOOT.js
@@ -371,3 +371,21 @@ function AjouterPlayer(playerName, selected, post) {
           <i class="fa-solid fa-trash"></i>
         </button>
       </div>
+        <div class="from-transparent rounded-b-xl text-white flex justify-center">
+        <div class="flex gap-2 justify-around font-semibold w-20 h-10 lg:text-[7px] hover:text-[7px] text-[4px] mb-10">
+          <div class="text-left">
+            <p> ${player.diving}DIV</p>
+            <p> ${player.handling}HND</p>
+            <p> ${player.kicking}KCK</p>
+        </div>
+        <div class="text-right">
+            <p> ${player.reflexes}RFX</p>
+            <p> ${player.speed}SPD</p>
+            <p> ${player.positioning}POS</p>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+                        `;
