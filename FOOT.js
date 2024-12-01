758902fee6c3195242df4acb1e9b3c5b6f96f6b7
hamzalhadouchi <hamzabniss16@gmail.com>
2024-12-01 19:29:21 +0100

    Create player input fields

--- FOOT.js
+++ FOOT.js
@@ -13,3 +13,36 @@ let passing = document.getElementById("playerPassing");
 let dribbling = document.getElementById("playerDribbling");
 let Defending = document.getElementById("playerDefending");
 let Physical = document.getElementById("playerPhysical");
+
+
+function addPlayer() {
+    if (!rigex()) {
+        return;
+    }
+
+    let player = {
+        name: Name.value,
+        photo: photo.value,
+        position: position.value,
+        club: club.value,
+        rating: rating.value,
+        pace: pace.value,
+        shooting: shooting.value,
+        passing: passing.value,
+        dribbling: dribbling.value,
+        defending: Defending.value,
+        physical: Physical.value,
+    };
+
+    let arryPlayer = JSON.parse(localStorage.getItem("players"));
+    console.log(localStorage.getItem("players"));
+
+    arryPlayer.push(player);
+    localStorage.setItem("players", JSON.stringify(arryPlayer));
+
+    console.log(arry);
+
+    console.log("done:", player);
+
+    clearInput();
+}
